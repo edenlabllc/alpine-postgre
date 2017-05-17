da99abf9428028104359a937c089bb6b8373eec3
Andrew Dryga <andrew@dryga.com>
2017-05-17 15:26:12 +0300

    Prepare DB for pghoard usage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,6 +42,28 @@ EOWARN
       authMethod=trust
     fi
 
+    # check password first so we can output the warning before postgres
+    # messes it up
+    if [ "${REPLICATOR_PASSWORD}" ]; then
+      repl_pass="PASSWORD '${REPLICATOR_PASSWORD}'"
+    else
+      # The - option suppresses leading tabs but *not* spaces. :)
+      cat >&2 <<'EOWARN'
+        ****************************************************
+        WARNING: No password has been set for the replicator.
+                 This will allow anyone with access to the
+                 Postgres port to access your database. In
+                 Docker's default configuration, this is
+                 effectively any other container on the same
+                 system.
+                 Use "-e REPLICATOR_PASSWORD=password" to set
+                 it in "docker run".
+        ****************************************************
+EOWARN
+
+      repl_pass=
+    fi
+
     { echo; echo "host all all 0.0.0.0/0 ${authMethod}"; } >> "${PGDATA}/pg_hba.conf"
 
     # internal start of server in order to allow set-up using psql-client
@@ -55,13 +77,18 @@ EOWARN
     echo "max_connections = 2048" >> "${PGDATA}/postgresql.conf"
     echo "shared_buffers = 512MB" >> "${PGDATA}/postgresql.conf"
 
+    # This will allow pghoard to backup this database
+    echo "wal_level = archive" >> "${PGDATA}/postgresql.conf"
+    echo "max_wal_senders = 4" >> "${PGDATA}/postgresql.conf"
+
     if [ "${POSTGRES_LOG_STATEMENTS}" ]; then
       echo "log_statement = '${POSTGRES_LOG_STATEMENTS}'" >> "${PGDATA}/postgresql.conf"
     fi
 
     : ${POSTGRES_USER:=postgres}
+    : ${REPLICATOR_USER:=pghoard}
     : ${POSTGRES_DB:=$POSTGRES_USER}
-    export POSTGRES_USER POSTGRES_DB
+    export POSTGRES_USER POSTGRES_DB REPLICATOR_USER
 
     psql=( psql -v ON_ERROR_STOP=1 )
 
@@ -78,6 +105,11 @@ EOSQL
     fi
     "${psql[@]}" --username postgres <<-EOSQL
       ${op} USER "${POSTGRES_USER}" WITH SUPERUSER ${pass} ;
+EOSQL
+    echo
+
+    "${psql[@]}" --username postgres <<-EOSQL
+      ${op} USER "${REPLICATOR_USER}" ${repl_pass} REPLICATION ;
 EOSQL
     echo
 
