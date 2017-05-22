f3c17a0ea374f7791eeece4ecb382597888a0446
Andrew Dryga <andrew@dryga.com>
2017-05-22 14:21:36 +0300

    Move to a official postgres image

---
+++ docker-entrypoint-initdb.d/replication.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+# check password first so we can output the warning before postgres
+# messes it up
+if [ "${REPLICATOR_PASSWORD}" ]; then
+  repl_pass="PASSWORD '${REPLICATOR_PASSWORD}'"
+  repl_authMethod=md5
+else
+  # The - option suppresses leading tabs but *not* spaces. :)
+  cat >&2 <<'EOWARN'
+    ****************************************************
+    WARNING: No password has been set for the replicator.
+             This will allow anyone with access to the
+             Postgres port to access your database. In
+             Docker's default configuration, this is
+             effectively any other container on the same
+             system.
+             Use "-e REPLICATOR_PASSWORD=password" to set
+             it in "docker run".
+    ****************************************************
+EOWARN
+
+  repl_pass=
+  repl_authMethod=trust
+fi
+
+{ echo; echo "host replication all 0.0.0.0/0 ${repl_authMethod}"; } >> "${PGDATA}/pg_hba.conf"
+
+# This will allow pghoard to backup this database
+echo "wal_level = archive" >> "${PGDATA}/postgresql.conf"
+echo "max_wal_senders = 4" >> "${PGDATA}/postgresql.conf"
+echo "max_replication_slots = 4" >> "${PGDATA}/postgresql.conf"
+
+: ${REPLICATOR_USER:=pghoard}
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER "${REPLICATOR_USER}" ${repl_pass} REPLICATION ;
+EOSQL
+
+export REPLICATOR_USER
